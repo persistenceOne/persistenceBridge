ba3a923179522c94f5ed361e98cc9164b4a84d28
avkr003 <abhinav@persistence.one>
2021-06-29 16:01:20 +0530

    merged with puneet code

--- application/db/status.go
+++ application/db/status.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"encoding/json"
-	"github.com/dgraph-io/badger/v3"
 )
 
 const (
@@ -68,37 +67,4 @@ func GetEthereumStatus() (Status, error) {
 
 func SetEthereumStatus(height int64) error {
 	return setStatus(ETHEREUM, height)
-}
-
-func GetValidators() ([]string, error) {
-	var status []string
-	err := db.Update(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte(VALIDATORS))
-		if err != nil {
-			return err
-		}
-		err = item.Value(func(val []byte) error {
-			err = json.Unmarshal(val, &status)
-			return err
-		})
-		return err
-	})
-	if err != nil {
-		return status, err
-	}
-	return status, nil
-}
-
-func SetValidators(validators []string) error {
-	b, err := json.Marshal(validators)
-	if err != nil {
-		return err
-	}
-	err = db.Update(func(txn *badger.Txn) error {
-		return txn.Set([]byte(VALIDATORS), b)
-	})
-	if err != nil {
-		return err
-	}
-	return nil
 }
--- application/db/validator.go
+++ application/db/validator.go
@@ -49,3 +49,37 @@ func DeleteValidator(address sdk.ValAddress) error {
 	validator.Address = address
 	return Delete(validator.Key())
 }
+
+func GetValidators() ([]sdk.ValAddress, error) {
+	var validators []sdk.ValAddress
+	err := iterateKeyValues(validator.GenerateStoreKey([]byte{}), func(key []byte, value []byte) error {
+		var v Validator
+		err := json.Unmarshal(value, &v)
+		if err != nil {
+			return err
+		}
+		validators = append(validators, v.Address)
+		return nil
+	})
+	if err != nil {
+		return validators, err
+	}
+	return validators, nil
+}
+
+func SetValidators(validators []string) error {
+	for _, v := range validators {
+		validatorAddress, err := sdk.ValAddressFromBech32(v)
+		if err != nil {
+			return err
+		}
+		err = SetValidator(Validator{
+			Address: validatorAddress,
+			Active:  true,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
