5f5bf1793a30d8ff59b985446ac5c4f50a3fb5cf
bahdotsh <appu.yess@gmail.com>
2021-08-05 17:46:55 +0530

    client_test modified

--- application/rpc/client_test.go
+++ application/rpc/client_test.go
@@ -4,137 +4,66 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/persistenceOne/persistenceBridge/application/db"
 	"github.com/stretchr/testify/require"
-	"log"
-	"net/rpc"
 	"testing"
 )
 
 func TestAddValidator(t *testing.T) {
 	validatorAddress, err := sdk.ValAddressFromBech32("cosmosvaloper156gqf9837u7d4c4678yt3rl4ls9c5vuursrrzf")
+
+	require.Equal(t, nil, err)
+
 	validatorName := "binance"
 	rpcEndpoint := "127.0.0.1:4040"
+	go StartServer(rpcEndpoint)
 	database, err := db.OpenDB("$HOME/persistence/persistenceBridge/application" + "/db")
+	validators, err2 := AddValidator(db.Validator{
+		Address: validatorAddress,
+		Name:   validatorName ,
+	}, rpcEndpoint)
 
-	if err != nil {
-		log.Printf("Db is already open: %v", err)
-		log.Printf("sending rpc")
-		validators, err2 := AddValidator(db.Validator{
-			Address: validatorAddress,
-			Name:   validatorName ,
-		}, rpcEndpoint)
-		require.Equal(t, nil, err2)
+	require.Equal(t, nil, err2)
 
-		var result []db.Validator
-		client, err := rpc.DialHTTP("tcp", rpcEndpoint)
-		defer client.Close()
-		require.Equal(t, nil, err)
+	defer database.Close()
+	validatorsGet, err2 := db.GetValidators()
 
-		err = client.Call("ValidatorRPC.AddValidator", db.Validator{
-			Address: validatorAddress,
-			Name:   validatorName ,
-		}, &result)
-
-		require.Equal(t, validators, result)
-
-	}else {
-		defer database.Close()
-		err2 := db.SetValidator(db.Validator{
-			Address: validatorAddress,
-			Name:    validatorName,
-		})
-		require.Equal(t, nil, err2)
-
-		validators, err2 := db.GetValidators()
-		require.Equal(t, nil, err2)
-
-		var result []db.Validator
-		client, err := rpc.DialHTTP("tcp", rpcEndpoint)
-		defer client.Close()
-		require.Equal(t, nil, err)
-
-		err = client.Call("ValidatorRPC.AddValidator", db.Validator{
-			Address: validatorAddress,
-			Name:   validatorName ,
-		}, &result)
-
-		require.Equal(t, validators, result)
-	}
+	require.Equal(t, nil, err2)
+	require.Equal(t, validators, validatorsGet)
 }
 
 func TestRemoveValidator(t *testing.T) {
 	validatorAddress, err := sdk.ValAddressFromBech32("cosmosvaloper156gqf9837u7d4c4678yt3rl4ls9c5vuursrrzf")
+
+	require.Equal(t, nil, err)
+
 	rpcEndpoint := "127.0.0.1:4040"
 	database, err := db.OpenDB("$HOME/persistence/persistenceBridge/application" + "/db")
 
-	if err != nil {
-		log.Printf("Db is already open: %v", err)
-		log.Printf("sending rpc")
-		validators, err2 := RemoveValidator(validatorAddress, rpcEndpoint)
-		require.Equal(t, nil, err2)
+	require.Equal(t, nil, err)
 
-		var result []db.Validator
-		client, err := rpc.DialHTTP("tcp", rpcEndpoint)
-		defer client.Close()
-		require.Equal(t, nil, err)
+	validators, err2 := RemoveValidator(validatorAddress, rpcEndpoint)
+	require.Equal(t, nil, err2)
 
-		err = client.Call("ValidatorRPC.DeleteValidator", validatorAddress, &result)
+	defer database.Close()
+	validatorsGet, err2 := db.GetValidators()
 
-		require.Equal(t, validators, result)
+	require.Equal(t, nil, err2)
+	require.Equal(t, validators, validatorsGet)
 
-	}else {
-		defer database.Close()
-		err = db.DeleteValidator(validatorAddress)
-
-		require.Equal(t, nil, err)
-
-		validators, err2 := db.GetValidators()
-		require.Equal(t, nil, err2)
-
-		var result []db.Validator
-		client, err := rpc.DialHTTP("tcp", rpcEndpoint)
-		defer client.Close()
-		require.Equal(t, nil, err)
-
-		err = client.Call("ValidatorRPC.DeleteValidator", validatorAddress, &result)
-
-
-		require.Equal(t, validators, result)
-	}
 }
 
 func TestShowValidators(t *testing.T) {
 	rpcEndpoint := "127.0.0.1:4040"
+
 	database, err := db.OpenDB("$HOME/persistence/persistenceBridge/application" + "/db")
+	require.Equal(t, nil, err)
 
-	if err != nil {
-		log.Printf("Db is already open: %v", err)
-		log.Printf("sending rpc")
-		validators, err2 := ShowValidators("", rpcEndpoint)
-		require.Equal(t, nil, err2)
+	validators, err2 := ShowValidators("", rpcEndpoint)
+	require.Equal(t, nil, err2)
 
-		var result []db.Validator
-		client, err := rpc.DialHTTP("tcp", rpcEndpoint)
-		defer client.Close()
-		require.Equal(t, nil, err)
+	defer database.Close()
+	validatorsGet, err2 := db.GetValidators()
 
-		err = client.Call("ValidatorRPC.GetValidators", "", &result)
+	require.Equal(t, nil, err2)
+	require.Equal(t, validators, validatorsGet)
 
-		require.Equal(t, validators, result)
-
-	}else {
-		defer database.Close()
-
-		validators, err2 := db.GetValidators()
-		require.Equal(t, nil, err2)
-
-		var result []db.Validator
-		client, err := rpc.DialHTTP("tcp", rpcEndpoint)
-		defer client.Close()
-		require.Equal(t, nil, err)
-
-		err = client.Call("ValidatorRPC.GetValidators", "", &result)
-
-
-		require.Equal(t, validators, result)
-	}
 }
