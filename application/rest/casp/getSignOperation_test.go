0dec23310be0fe2b6ca9487e40380a560adfd6e1
bahdotsh <appu.yess@gmail.com>
2021-08-09 13:42:43 +0530

    added getSignOperation_test

---
+++ application/rest/casp/getSignOperation_test.go
@@ -0,0 +1,31 @@
+package casp
+
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/persistenceOne/persistenceBridge/application/configuration"
+	"github.com/stretchr/testify/require"
+	"log"
+	"path/filepath"
+	"testing"
+)
+
+var ethBridgeAdmin common.Address
+
+func TestGetSignOperation(t *testing.T) {
+
+	pStakeConfig := configuration.InitConfig()
+	_, err := toml.DecodeFile(filepath.Join("/Users/gokuls/.persistenceBridge/", "config.toml"), &pStakeConfig)
+	if err != nil {
+		log.Fatalf("Error decoding pStakeConfig file: %v\n", err.Error())
+	}
+
+	operationID := "69544933-2767-4e09-af4a-c2dacb9a20af"
+	reponseRecieved, err := GetSignOperation(operationID)
+
+	require.Equal(t, nil, err)
+	require.Equal(t, reponseRecieved.StatusText, "Completed")
+	require.Equal(t, reponseRecieved.IsApproved, true)
+	require.Equal(t, reponseRecieved.AccountID, "bd4c618e-8046-4fef-bdaa-9716ade77553")
+
+}
