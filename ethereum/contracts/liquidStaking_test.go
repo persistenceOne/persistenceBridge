a56fd2c6ebde9502fb237c33a1fe2614dabfc95f
bahdotsh <appu.yess@gmail.com>
2021-08-11 14:30:48 +0530

    liquid staking test added

--- ethereum/contracts/liquidStaking_test.go
+++ ethereum/contracts/liquidStaking_test.go
@@ -1,1 +1,51 @@
 package contracts
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/persistenceOne/persistenceBridge/application/casp"
+	"github.com/persistenceOne/persistenceBridge/application/configuration"
+	"github.com/persistenceOne/persistenceBridge/utilities/config"
+	"github.com/stretchr/testify/require"
+	"math/big"
+	"testing"
+)
+
+
+
+func Test_onStake(t *testing.T) {
+	configuration.InitConfig()
+	appconfig := config.SetConfig()
+	configuration.SetConfig(&appconfig)
+	tmAddress, err := casp.GetTendermintAddress()
+	require.Equal(t, nil, err)
+
+	configuration.SetPStakeAddress(tmAddress)
+
+	i := new(big.Int)
+	i.SetInt64(1000)
+	arr := []interface{}{common.BytesToAddress([]byte("0x477573f212a7bdd5f7c12889bd1ad0aa44fb82aa")),  i}
+	stakeMsg, ercAddress, err := onStake(arr)
+	require.Equal(t, nil, err)
+	require.Equal(t, common.BytesToAddress([]byte("0x477573f212a7bdd5f7c12889bd1ad0aa44fb82aa")).String(), ercAddress.String())
+	stakeMsgString := stakeMsg.String()
+	require.NotNil(t, stakeMsgString)
+}
+
+func Test_onUnStake(t *testing.T) {
+	configuration.InitConfig()
+	appconfig := config.SetConfig()
+	configuration.SetConfig(&appconfig)
+	tmAddress, err := casp.GetTendermintAddress()
+	require.Equal(t, nil, err)
+
+	configuration.SetPStakeAddress(tmAddress)
+
+	i := new(big.Int)
+	i.SetInt64(1000)
+	arr := []interface{}{common.BytesToAddress([]byte("0x477573f212a7bdd5f7c12889bd1ad0aa44fb82aa")),  i}
+	UnStakeMsg, ercAddress, err := onUnStake(arr)
+	require.Equal(t, nil, err)
+	require.Equal(t, common.BytesToAddress([]byte("0x477573f212a7bdd5f7c12889bd1ad0aa44fb82aa")).String(), ercAddress.String())
+	UnStakeMsgString := UnStakeMsg.String()
+	require.NotNil(t, UnStakeMsgString)
+}
