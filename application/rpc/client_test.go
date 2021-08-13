f3a62cc4b5d1c97fb9ae770d4616566200e9d3d0
bahdotsh <appu.yess@gmail.com>
2021-08-13 14:51:38 +0530

    client_test modified

--- application/rpc/client_test.go
+++ application/rpc/client_test.go
@@ -4,6 +4,8 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/persistenceOne/persistenceBridge/application/db"
 	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -11,11 +13,13 @@ import (
 
 func TestRemoveValidator(t *testing.T) {
 	validatorAddress, err := sdk.ValAddressFromBech32("cosmosvaloper156gqf9837u7d4c4678yt3rl4ls9c5vuursrrzf")
+	dirname, err := os.UserHomeDir()
 
 	require.Equal(t, nil, err)
 	validatorName := "binance"
 	rpcEndpoint := "127.0.0.1:4040"
-	database, err := db.OpenDB("$HOME/persistence/persistenceBridge/application" + "/db")
+	database, err := db.OpenDB(filepath.Join(dirname, "/persistence/persistenceBridge/application") + "/db")
+	defer database.Close()
 
 	require.Equal(t, nil, err)
 	validators, err2 := AddValidator(db.Validator{
@@ -26,7 +30,6 @@ func TestRemoveValidator(t *testing.T) {
 	validators, err2 = RemoveValidator(validatorAddress, rpcEndpoint)
 	require.Equal(t, nil, err2)
 
-	defer database.Close()
 	validatorsGet, err2 := db.GetValidators()
 
 	require.Equal(t, nil, err2)
@@ -36,14 +39,16 @@ func TestRemoveValidator(t *testing.T) {
 
 func TestShowValidators(t *testing.T) {
 	rpcEndpoint := "127.0.0.1:4040"
+	dirname, err := os.UserHomeDir()
 
-	database, err := db.OpenDB("$HOME/persistence/persistenceBridge/application" + "/db")
+	database, err := db.OpenDB(filepath.Join(dirname, "/persistence/persistenceBridge/application") + "/db")
+	defer database.Close()
+
 	require.Equal(t, nil, err)
 
 	validators, err2 := ShowValidators("", rpcEndpoint)
 	require.Equal(t, nil, err2)
 
-	defer database.Close()
 	validatorsGet, err2 := db.GetValidators()
 
 	require.Equal(t, nil, err2)
