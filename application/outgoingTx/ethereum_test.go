26664bd65337d92fca6cad90440db1607c4542f0
Ankit Kumar <ankit.kr554@gmail.com>
2021-08-13 16:22:20 +0530

    updated outgoingTX unit tests

--- application/outgoingTx/ethereum_test.go
+++ application/outgoingTx/ethereum_test.go
@@ -2,6 +2,7 @@ package outgoingTx
 
 import (
 	"context"
+	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
@@ -11,6 +12,9 @@ import (
 	"github.com/persistenceOne/persistenceBridge/application/configuration"
 	"github.com/persistenceOne/persistenceBridge/ethereum/abi/tokenWrapper"
 	"math/big"
+	"os"
+	"reflect"
+	"regexp"
 	"strings"
 
 	"github.com/stretchr/testify/require"
@@ -21,32 +25,37 @@ import (
 
 func TestEthereumWrapToken(t *testing.T) {
 	pStakeConfig := configuration.InitConfig()
-	_, err := toml.DecodeFile(filepath.Join("/Users/ankitkumar/.persistenceBridge/", "config.toml"), &pStakeConfig)
+	dirname, _ := os.UserHomeDir()
+	_, err := toml.DecodeFile(filepath.Join(dirname, "/.persistenceBridge/config.toml"), &pStakeConfig)
 	if err != nil {
 		log.Fatalf("Error decoding pStakeConfig file: %v\n", err.Error())
 	}
 	ethaddress, _ := casp.GetEthAddress()
 	wrapTokenMsg := []WrapTokenMsg{{
 		Address: ethaddress,
-		Amount:  &big.Int{},},
+		Amount:  &big.Int{}},
 	}
 
-	ethereumClient, error_in_client := ethclient.Dial(pStakeConfig.Ethereum.EthereumEndPoint)
-	if error_in_client != nil {
+	ethereumClient, errorInClient := ethclient.Dial(pStakeConfig.Ethereum.EthereumEndPoint)
+	if errorInClient != nil {
 		t.Errorf("Error getting ETH client!")
 	}
 	ethWrapToken, error := EthereumWrapToken(ethereumClient,wrapTokenMsg)
 	if error != nil {
 		t.Errorf("Failed getting ETH wrap token for: %v",ethaddress)
 	}
+	re := regexp.MustCompile(`0x[0-9a-fA-F]{64}`)
 	require.NotNil(t, ethWrapToken)
+	require.Equal(t, true, re.MatchString(ethWrapToken.String()))
+	require.Equal(t, reflect.TypeOf(common.Hash{}),reflect.TypeOf(ethWrapToken))
 	require.NotEqual(t, nil,ethWrapToken)
 	require.Equal(t,32, len(ethWrapToken))
 }
 
 func Test_sendTxToEth(t *testing.T){
 	pStakeConfig := configuration.InitConfig()
-	_, err := toml.DecodeFile(filepath.Join("/Users/ankitkumar/.persistenceBridge/", "config.toml"), &pStakeConfig)
+	dirname, _ := os.UserHomeDir()
+	_, err := toml.DecodeFile(filepath.Join(dirname, "/.persistenceBridge/config.toml"), &pStakeConfig)
 	if err != nil {
 		log.Fatalf("Error decoding pStakeConfig file: %v\n", err.Error())
 	}
@@ -68,6 +77,9 @@ func Test_sendTxToEth(t *testing.T){
 	if err != nil {
 		t.Errorf("Error sending TX to ETH with error: \n %v",err)
 	}
+	re := regexp.MustCompile(`0x[0-9a-fA-F]{64}`)
+	require.Equal(t, true, re.MatchString(txToETHhash.String()))
+	require.Equal(t, reflect.TypeOf(common.Hash{}),reflect.TypeOf(txToETHhash))
 	require.NotNil(t, txToETHhash)
 	require.LessOrEqual(t, 0, len(txToETHhash))
 	require.Equal(t, 32, len(txToETHhash))
@@ -75,7 +87,8 @@ func Test_sendTxToEth(t *testing.T){
 
 func Test_getEthSignature(t *testing.T) {
 	pStakeConfig := configuration.InitConfig()
-	_, err := toml.DecodeFile(filepath.Join("/Users/ankitkumar/.persistenceBridge/", "config.toml"), &pStakeConfig)
+	dirname, _ := os.UserHomeDir()
+	_, err := toml.DecodeFile(filepath.Join(dirname, "/.persistenceBridge/config.toml"), &pStakeConfig)
 	if err != nil {
 		log.Fatalf("Error decoding pStakeConfig file: %v\n", err.Error())
 	}
@@ -110,6 +123,11 @@ func Test_getEthSignature(t *testing.T) {
 	if errorGettingSignature != nil {
 		t.Errorf("")
 	}
+	fmt.Println(signatureResponse)
+	require.Equal(t, reflect.TypeOf([]byte{}),reflect.TypeOf(ethSignature))
+	require.Equal(t, reflect.TypeOf(0),reflect.TypeOf(signatureResponse))
+	require.Equal(t, 64, len(ethSignature))
+	//require.Equal(t, 0,signatureResponse,"Wrong Signature Response") //signatureResponseVaries 0/1
 	require.NotEqual(t, -1,signatureResponse)
 	require.NotNil(t,ethSignature )
 }
@@ -117,10 +135,15 @@ func Test_getEthSignature(t *testing.T) {
 
 func Test_setEthBridgeAdmin(t *testing.T) {
 	pStakeConfig := configuration.InitConfig()
-	_, err := toml.DecodeFile(filepath.Join("/Users/ankitkumar/.persistenceBridge/", "config.toml"), &pStakeConfig)
+	dirname, _ := os.UserHomeDir()
+	_, err := toml.DecodeFile(filepath.Join(dirname, "/.persistenceBridge/config.toml"), &pStakeConfig)
 	if err != nil {
 		log.Fatalf("Error decoding pStakeConfig file: %v\n", err.Error())
 	}
 	ethBridgeAdminErro := setEthBridgeAdmin()
+	re := regexp.MustCompile(`^0x[0-9a-fA-F]{40}$`)
+	require.Equal(t, true,re.MatchString(ethBridgeAdmin.String()))
 	require.Nil(t, nil,ethBridgeAdminErro,"Eth Admin setting failed")
+	require.NotEqual(t, "0x0000000000000000000000000000000000000000",ethBridgeAdmin,"ETH Bridge Admin alreadu set")
+	require.Equal(t, nil,ethBridgeAdminErro)
 }
