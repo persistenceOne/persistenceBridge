09ea5e71b6bd87c6765d95bea8f85b112516137d
Ankit Kumar <ankit.kr554@gmail.com>
2021-08-17 17:06:35 +0530

    added unit tests for tendermint [NOT WORKING]

--- tendermint/chain_test.go
+++ tendermint/chain_test.go
@@ -1,1 +1,34 @@
 package tendermint
+
+import (
+	"github.com/cosmos/relayer/relayer"
+	"github.com/stretchr/testify/require"
+	"os"
+	"reflect"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+func TestInitializeAndStartChain(t *testing.T) {
+	dirname, _ := os.UserHomeDir()
+	fileName := strings.Join([]string{dirname,"/.persistenceBridge/chain.json"},"")
+	initAndStartChain, errInit := InitializeAndStartChain(fileName, "336h",dirname)
+	if errInit != nil {
+		t.Errorf("Error Initializing Chain: %v",errInit)
+	}
+	re := regexp.MustCompile(`^cosmos$`)
+	require.Equal(t, true,re.MatchString(initAndStartChain.AccountPrefix))
+	require.Equal(t, reflect.TypeOf(&relayer.Chain{}),reflect.TypeOf(initAndStartChain))
+	require.NotNil(t,initAndStartChain )
+}
+
+func Test_fileInputAdd(t *testing.T) {
+	dirname, _ := os.UserHomeDir()
+	fileName := strings.Join([]string{dirname,"/.persistenceBridge/chain.json"},"")
+	fileIPadd, err := fileInputAdd(fileName)
+	if err != nil {
+		t.Errorf("Failed Reading config File")
+	}
+	require.NotNil(t, fileIPadd)
+}
--- tendermint/listener_test.go
+++ tendermint/listener_test.go
@@ -1,1 +1,37 @@
 package tendermint
+
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/persistenceOne/persistenceBridge/application/configuration"
+	"github.com/persistenceOne/persistenceBridge/application/constants"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestStartListening(t *testing.T) {
+	dirname, _ := os.UserHomeDir()
+	fileName := strings.Join([]string{dirname,"/.persistenceBridge/chain.json"},"")
+	initAndStartChain, errInit := InitializeAndStartChain(fileName, "336h",dirname)
+	pStakeConfig := configuration.InitConfig()
+	_, err := toml.DecodeFile(filepath.Join(dirname, "/.persistenceBridge/config.toml"), &pStakeConfig)
+	if err != nil {
+		log.Fatalf("Error decoding pStakeConfig file: %v\n", err.Error())
+	}
+	if errInit != nil {
+		t.Errorf("Error Initializing Chain: %v",errInit)
+	}
+	initClientCtx := client.Context{}.
+		WithHomeDir(constants.DefaultPBridgeHome)
+	protoCodec := codec.NewProtoCodec(initClientCtx.InterfaceRegistry)
+	StartListening(initClientCtx.WithHomeDir(dirname), initAndStartChain, pStakeConfig.Kafka.Brokers, protoCodec, time.Duration(200)*time.Millisecond)
+}
+
+func Test_getAllTxResults(t *testing.T) {
+
+}
