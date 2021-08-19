3c650d05e117ecf051821165f9afa06ec151506e
Ankit Kumar <ankit.kr554@gmail.com>
2021-08-19 15:07:15 +0530

    added unit tests for tendermint modules

---
+++ tendermint/onNewBlock_test.go
@@ -0,0 +1,9 @@
+package tendermint
+
+import (
+	"testing"
+)
+
+func Test_onNewBlock(t *testing.T) {
+	//ctx context.Context, clientCtx client.Context, chain *relayer.Chain, kafkaProducer *sarama.SyncProducer, protoCodec *codec.ProtoCodec
+}
