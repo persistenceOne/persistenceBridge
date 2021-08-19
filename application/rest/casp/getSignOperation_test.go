d350b32ae5e1d1ed4fc773eb3ec5451209b49329
bahdotsh <appu.yess@gmail.com>
2021-08-19 11:26:40 +0530

    getSignOperationTest updated

--- application/rest/casp/getSignOperation_test.go
+++ application/rest/casp/getSignOperation_test.go
@@ -1,14 +1,11 @@
 package casp
 
 import (
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/persistenceOne/persistenceBridge/application/configuration"
 	test "github.com/persistenceOne/persistenceBridge/utilities/testing"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
-
-var ethBridgeAdmin common.Address
 
 func TestGetSignOperation(t *testing.T) {
 	configuration.InitConfig()
@@ -16,11 +13,15 @@ func TestGetSignOperation(t *testing.T) {
 	configuration.SetConfig(&appconfig)
 
 	operationID := "69544933-2767-4e09-af4a-c2dacb9a20af"
-	reponseRecieved, err := GetSignOperation(operationID)
+	responseRecieved, err := GetSignOperation(operationID)
 
 	require.Equal(t, nil, err)
-	require.Equal(t, reponseRecieved.StatusText, "Completed")
-	require.Equal(t, reponseRecieved.IsApproved, true)
-	require.Equal(t, reponseRecieved.AccountID, "bd4c618e-8046-4fef-bdaa-9716ade77553")
+	require.Equal(t, responseRecieved.StatusText, "Completed")
+	require.Equal(t, responseRecieved.IsApproved, true)
+	require.Equal(t, responseRecieved.AccountID, "bd4c618e-8046-4fef-bdaa-9716ade77553")
+
+	_, err = GetSignOperation("")
+	require.Equal(t, "Operation not found", err.Error())
+
 
 }
