ade3b599e0d2033a02770b7c0ad6c3d8c7b6260d
avkr003 <abhinav@persistence.one>
2021-10-08 12:22:41 +0530

    Fixing test case

--- application/casp/signing_test.go
+++ application/casp/signing_test.go
@@ -27,10 +27,10 @@ func TestGetCASPSignature(t *testing.T) {
 
 func TestSendDataToSign(t *testing.T) {
 	dataToSign := []string{"55C53F5D490297900CEFA825D0C8E8E9532EE8A118ABE7D8570762CD38BE9818"}
-	publickey := []string{"3056301006072A8648CE3D020106052B8104000A0342000413109ECEADCBF6122EF44184B207F8C6820E509497792DDFB166BC090A0FB4447CFFCE16BAAF9EC7F57D14C02641B3A6A698614D973ED744E725A85E62535DA4"}
-
 	configuration.InitConfig()
 	configuration.SetConfig(test.GetCmdWithConfig())
+
+	publickey := []string{configuration.GetAppConfig().CASP.TendermintPublicKey}
 
 	caspSignatureOperationID, err := SendDataToSign(dataToSign, publickey, false)
 	require.Nil(t, err, "Error getting casp signing OperationID")
