1318af1f1066f1a016d20c5d5cc8f49dd86f48cf
puneetmahajan <puneet@persistence.one>
2021-10-13 14:19:51 +0530

    removing unwanted kafka utils.

--- kafka/utils/types.go
+++ kafka/utils/types.go
@@ -9,28 +9,6 @@ import (
 	"github.com/Shopify/sarama"
 )
 
-// Ticket : is a type that implements string
-type Ticket string
-
-//// KafkaMsg : is a store that can be stored in kafka queues
-//type KafkaMsg struct {
-//	MsgBytes      sdk.MsgBytes `json:"msg"`
-//	TicketID Ticket  `json:"ticketID"`
-//}
-//
-//// NewKafkaMsgFromRest : makes a msg to send to kafka queue
-//func NewKafkaMsgFromRest(msg sdk.MsgBytes, ticketID Ticket) KafkaMsg {
-//	return KafkaMsg{
-//		MsgBytes:      msg,
-//		TicketID: ticketID,
-//	}
-//}
-
-// TicketIDResponse : is a json structure to send TicketID to user
-type TicketIDResponse struct {
-	TicketID Ticket `json:"ticketID" valid:"required~ticketID is mandatory,length(20)~ticketID length should be 20" `
-}
-
 // KafkaState : is a struct showing the state of kafka
 type KafkaState struct {
 	HomeDir       string
