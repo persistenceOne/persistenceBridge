2a0f3fc920c62fdad5fbcccd0ed8324240aaed28
puneetmahajan <puneet@persistence.one>
2021-10-19 15:27:18 +0530

    disabling remove validator if number of validators in db <= 1.

--- application/commands/removeValidator.go
+++ application/commands/removeValidator.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"errors"
+	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	constants2 "github.com/persistenceOne/persistenceBridge/application/constants"
 	"github.com/persistenceOne/persistenceBridge/application/db"
@@ -54,33 +55,42 @@ func RemoveCommand() *cobra.Command {
 			if err != nil {
 				log.Printf("Db is already open: %v", err)
 				log.Printf("sending rpc")
+
 				var err2 error
+				validators, err2 = rpc.ShowValidators("", rpcEndpoint)
+				if err2 != nil {
+					return err2
+				}
+				if len(validators) <= 1 {
+					return errors.New(fmt.Sprintf("Cannot remove any more validators, total validators: %v", len(validators)))
+				}
+
 				validators, err2 = rpc.RemoveValidator(validatorAddress, rpcEndpoint)
 				if err2 != nil {
 					return err2
 				}
 			} else {
 				defer database.Close()
-				err = db.DeleteValidator(validatorAddress)
-				if err != nil {
-					return err
-				}
 
 				var err2 error
 				validators, err2 = db.GetValidators()
 				if err2 != nil {
 					return err2
 				}
+				if len(validators) <= 1 {
+					return errors.New(fmt.Sprintf("Cannot remove any more validators, total validators: %v", len(validators)))
+				}
+
+				err = db.DeleteValidator(validatorAddress)
+				if err != nil {
+					return err
+				}
 
 			}
-			if len(validators) == 0 {
-				log.Println("IMPORTANT: No validator present to redelegate!!!")
-				return errors.New("need to have at least one validator to redelegate to")
-			} else {
-				log.Printf("Total validators %d:\n", len(validators))
-				for i, validator := range validators {
-					log.Printf("%d. %s - %s\n", i+1, validator.Name, validator.Address.String())
-				}
+
+			log.Printf("Total validators %d:\n", len(validators))
+			for i, validator := range validators {
+				log.Printf("%d. %s - %s\n", i+1, validator.Name, validator.Address.String())
 			}
 
 			time.Sleep(1 * time.Minute)
