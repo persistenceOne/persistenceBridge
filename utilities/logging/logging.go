7c5503320783af48ccbe4e6b71d735d7e9b17da1
bahdotsh <appu.yess@gmail.com>
2022-07-29 11:52:47 +0400

    test

--- utilities/logging/logging.go
+++ utilities/logging/logging.go
@@ -35,7 +35,7 @@ func InitializeBot() (err error) {
 
 	values := map[string]string{"name": "John Doe", "occupation": "gardener"}
 	json_data, err := json.Marshal(values)
-	resp, err := http.Post("https://hooks.slack.com/services/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", "application/json",
+	resp, err := http.Post("https://hooks.slack.com/services/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", "application/json",
 		bytes.NewBuffer(json_data))
 
 	if err != nil {
